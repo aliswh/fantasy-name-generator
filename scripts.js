5146d4fdd6bfa5fd1551924308b73697f6110b81
Alice S <39992505+aliswh@users.noreply.github.com>
2021-04-12 12:35:54 +0200

    generator function fix
    prepare for brython set up

--- scripts.js
+++ scripts.js
@@ -3,9 +3,26 @@ tf.loadLayersModel("model/model.json").then(function(model) {
     window.model = model;
 });
 
+
+async function load(){
+    const model = await tf.loadLayersModel('model/model.json');
+    return model.summary()
+  };
+//var loadedModel = await tf.loadLayersModel('https://github.com/aliswh/fantasy-name-generator/blob/main/model/model.json');
+
+function predict(model) {
+    const model = load();
+    const tensor = tf.tensor();
+    model.then(model => {
+        let result = model.predict(tensor);
+        result = result.dataSync()[0];
+        return result;
+    });
+};
+
 function generate(){
-    document.getElementById("name").value = 'my model output';
-}
+    document.getElementById("name").value = predict();
+};
 
 function copy(){
     var text = document.getElementById('name');
@@ -14,4 +31,4 @@ function copy(){
     document.execCommand("copy");
     /* Alert the copied text */
     alert("Name copied!");
-}
+};
